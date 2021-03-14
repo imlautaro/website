a41e5cd796ec8a20e7ea2e6c6fd2918206bb448e
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-03-14 20:07:34 -0300

    Updated send-message function

--- api/send-message.js
+++ api/send-message.js
@@ -3,10 +3,8 @@ const emailValidator = require('email-validator')
 const nodemailer = require('nodemailer')
 
 export default (req, res) => {
-	console.log(req.body)
 	if (req.body.name && req.body.email && req.body.message) {
 		if (emailValidator.validate(req.body.email)) {
-			console.log(process.env.GMAIL_USER)
 			const transporter = nodemailer.createTransport({
 				service: 'gmail',
 				auth: {
@@ -26,7 +24,6 @@ export default (req, res) => {
 			}
 			transporter.sendMail(mailOptions, (err, info) => {
 				if (err) {
-					console.error(err)
 					res.status(400).send('Ha ocurrido un error.')
 				} else {
 					res.status(201).send('Correo enviado.')
